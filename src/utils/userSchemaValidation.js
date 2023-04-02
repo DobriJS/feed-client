f822aa8c1fa02d05cdd87d36e312ae9868745ef9
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-04-02 23:34:16 +0300

    Add detailed user validation

--- src/utils/userSchemaValidation.js
+++ src/utils/userSchemaValidation.js
@@ -3,15 +3,20 @@ import * as yup from 'yup';
 export const userRegistrationSchema = yup.object().shape({
   username: yup
     .string()
-    .min(4)
-    .max(12)
-    .required('Username is required with min 4 and max 12 chars'),
+    .min(4, 'Username must be at least 4 chars')
+    .max(12, 'Username must be less than 12 chars')
+    .required('Username is required'),
   password: yup
     .string()
-    .min(3)
-    .max(12)
+    .min(5, 'Password must be at least 5 chars')
+    .max(12, 'Password must be less than 12 chars')
     .required('Password is required with min 3 and max 12 chars'),
-  email: yup.string().email().max(16).required('Email is required'),
+  email: yup
+    .string()
+    .email()
+    .min(6, 'Email must be at least 6 chars')
+    .max(20, 'Email must be less than 20 chars')
+    .required('Email is required'),
   pic: yup
     .string()
     .url()
